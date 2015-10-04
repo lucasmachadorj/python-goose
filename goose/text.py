5bdaf30af34756914765d46662a3acfcdb4e9993
lucasmachadorj <lucasmachado.rj@gmail.com>
2015-10-04 01:36:19 -0300

    add sys package

--- goose/text.py
+++ goose/text.py
@@ -21,6 +21,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from __future__ import absolute_import
+import sys
 import six
 import os
 import re
